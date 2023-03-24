5d962276b3244dc101bd711f44527e65dde1b453
ELLY AKWOYO <iamopetu@gmail.com>
2023-03-25 01:07:30 +0300

    seconde

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/index', function () {
+Route::get('/', function () {
     return view('index');
 });
